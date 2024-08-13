a96f9c4e01c891f5ee086a5515029fbcbc517b0f
Daniel Rosehill <daniel@danielrosehill.co.il>
2024-08-13 18:17:42 +0300

    script

--- Images/watermarking/watermark-and-overwrite.sh
+++ Images/watermarking/watermark-and-overwrite.sh
@@ -7,7 +7,7 @@ read -p "Enter the input directory: " input_dir
 watermark="/home/daniel/Images/Watermarking/watermarks/latest.png"
 
 # Loop through all image files in the input directory
-for img in "$input_dir"/*.{jpg,jpeg,png,gif}; do
+for img in "$input_dir"/*.{jpg,jpeg,png,gif,webp}; do
     # Check if the file exists
     if [ -f "$img" ]; then
         # Add the watermark to the bottom right, 300px from the edges
